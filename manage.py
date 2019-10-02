715aa36f01e4a0530b6dd0a8c4c2a0f5adfa52ad
SSILHR\zrehman <zrehman@ssidecisions.com>
2019-10-02 11:22:41 +0500

    Pushing to test if the code will build with errors

--- manage.py
+++ manage.py
@@ -2,5 +2,5 @@ from app import app as application
 from app.blueprints import blueprint
 
 application.register_blueprint(blueprint)
-if __name__ == "__main__":
+if __name__ == "__main__"
     application.run(debug=True)
